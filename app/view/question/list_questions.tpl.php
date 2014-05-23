07c3a088b5d48e6be19694e4267457f7ba17ab2b
Emil Nordqvist <emil@nordq.se>
2014-05-23 11:49:44 +0200

    New style for questions list

--- app/view/question/list_questions.tpl.php
+++ app/view/question/list_questions.tpl.php
@@ -1,25 +1,27 @@
 <h1>Todays Quests</h1>
 <?php foreach($questions as $question) : ?>
-	
-	<h1 class="question-title-front"><a href="<?= $this->url->create('questions/id/' . $question->id); ?>"><?= $question->title; ?></a></h1>
-
-	<div class="views">
-		<p>Views</p>
-		<?php if ($question->views == 0){echo "0";} else {echo $question->views;} ?>
-	</div>
-	
-	<div class="answers">
-		<p>Answers</p>
-		<?php if ($question->answersCount == 0){echo "0";} else {echo $question->answersCount;} ?>
-	</div>
-	<?php if (isset($question->tags)): ?>
-		<div class="tags">
-			<?php foreach ($question->tags as $tag): ?>
-				<a href="<?= $this->url->create('tags/' . $tag->tag_name); ?>" class="tag-link"><?= $tag->tag_name; ?></a>
-			<?php endforeach ?>
+	<div class="question">
+		<div class="views">
+			<p>Views</p>
+			<?php if ($question->views == 0){echo "0";} else {echo $question->views;} ?>
 		</div>
-	<?php endif ?>
+		
+		<div class="answers">
+			<p>Answers</p>
+			<?php if ($question->answersCount == 0){echo "0";} else {echo $question->answersCount;} ?>
+		</div>
+
+		<h1 class="title-front"><a href="<?= $this->url->create('questions/id/' . $question->id); ?>"><?= $question->title; ?></a></h1>
+
+
+		<?php if (isset($question->tags)): ?>
+			<div class="tags">
+				<?php foreach ($question->tags as $tag): ?>
+					<a href="<?= $this->url->create('tags/' . $tag->tag_name); ?>" class="tag-link"><?= $tag->tag_name; ?></a>
+				<?php endforeach ?>
+			</div>
+		<?php endif ?>
+	</div>
 <?php endforeach; ?> 
 
-<hr>
-<a href="<?= $this->url->create('questions/new'); ?>">New Quest</a>
+<p class="new-question"><a href="<?= $this->url->create('questions/new'); ?>">New Quest</a></p>
