52a0945b5658585bff93a0ef62ec9cb055cca94d
Emil Nordqvist <emil@nordq.se>
2014-08-03 14:07:44 +0200

    Improved Users list view for longer names.

--- app/view/users/list-all.tpl.php
+++ app/view/users/list-all.tpl.php
@@ -3,11 +3,18 @@
         <h5>Ordered by Points</h5>
 
 		<?php foreach ($users as $user) : ?>
+		
 			<div class="mini-profile left">
-			<img class="gravatar left" src="<?= $this->mzHelpers->get_gravatar($user->email, 128); ?>" alt=""><br>
-			<a href="<?= $this->url->create('users/id/' . $user->id ); ?>"><?= $user->username ?></a> <br>
-			Points <?= $user->points ?> <br>
+               
+                <img class="gravatar left" src="<?= $this->mzHelpers->get_gravatar($user->email, 128); ?>" alt="">
+
+                <div class="info">
+                    <a href="<?= $this->url->create('users/id/' . $user->id ); ?>"><?= $user->username ?></a> <br>
+                    Points: <?= $user->points ?>
+                </div>
+            
             </div>
+            
 		<?php endforeach; ?>
 
 
